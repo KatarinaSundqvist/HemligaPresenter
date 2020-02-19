fa13e3ed0b4dc0b53315e7319473ea6828a79ad8
KatarinaSundqvist <singsongkat@live.com>
2020-02-19 17:16:08 +0100

    Added basic models and created a page for group creation

---
+++ HemligaPresenter/HemligaPresenter/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HemligaPresenter.Models {
+    public class Group {
+        public int GroupId { get; set; }
+        public string GroupName { get; set; }
+        public DateTime Celebration { get; set; }
+        public Decimal MaxSpend { get; set; }
+        public List<Member> Members { get; set; }
+        public string GroupToken { get; set; } // TODO: To be created automatically when group is created. See also Member
+    }
+}
