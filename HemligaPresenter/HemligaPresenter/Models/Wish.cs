914bbec2ec5829a537dcd0b9d580f542d1c969ea
KatarinaSundqvist <singsongkat@live.com>
2020-02-25 11:28:18 +0100

    Enabled nullable reference types

--- HemligaPresenter/HemligaPresenter/Models/Wish.cs
+++ HemligaPresenter/HemligaPresenter/Models/Wish.cs
@@ -4,5 +4,6 @@
         public string ItemName { get; set; }
         public string Link { get; set; }
         public string Description { get; set; }
+        public bool StillOpen { get; set; }
     }
 }
