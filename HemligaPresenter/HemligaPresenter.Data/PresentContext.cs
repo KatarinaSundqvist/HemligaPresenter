1a9a607866890e09a87c3a2907fcb38983a8d901
KatarinaSundqvist <singsongkat@live.com>
2020-10-08 13:21:54 +0200

    Added a context class

---
+++ HemligaPresenter/HemligaPresenter.Data/PresentContext.cs
@@ -0,0 +1,8 @@
+﻿using HemligaPresenter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HemligaPresenter.Data {
+    public sealed class PresentContext : DbContext {
+        public DbSet<Wish> MyProperty { get; set; }
+    }
+}
